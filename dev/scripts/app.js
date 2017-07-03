05d1f31bd2a82ffaed75c5b16083cea50e8acc6c
Dziuba88 <Dziuba88@Gmail.com>
2017-07-03 19:30:09 +0300

    changes 3/07/2017

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -33,4 +33,36 @@ $('.owl-carousel').owlCarousel({
 });
 
 $("#contact-form").validate();
+
 $("#comment-form").validate();
+
+if($("[type=tel]").length>0) {$("[type=tel]").mask("+(999) 999-9999", {placeholder:"+(___) ___-____"})};
+
+
+$.each( $( ".image-to-bg" ),function(){
+      var cssValues = {
+        "background":"url(" + $(this).attr("src") + ") no-repeat center center scroll",
+        "background-size":"cover"
+      };
+      $(this).hide();
+      $(this).parent().css(cssValues);
+});
+
+    var hashTagActive = "";
+    $(".scroll-more").on("click touchstart" , function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 600, 'linear');
+            hashTagActive = this.hash;
+        }
+    });
--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,3 +2,4 @@
 //@prepros-append components/owl.carousel.js
 //@prepros-append components/owl.carousel2.thumbs.js
 //@prepros-append components/jquery.validate.js
+//@prepros-append components/jquery.maskedinput.js
